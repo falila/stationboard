18bf6b69fca901c832fa504435d644bb1d849361
falila <soulfa23@gmail.cm>
2021-01-24 20:06:40 -0500

    test auth endpoint added authorization

--- tests/test_station_endpoints.py
+++ tests/test_station_endpoints.py
@@ -1,18 +1,5 @@
-from app import create_app
-from config import TestingConfig
 from app.model import Station
-import pytest
 import json
-
-
-@pytest.fixture(scope='module')
-def my_test_client():
-    app = create_app(TestingConfig)
-
-    # Creating our testing client
-    with app.test_client() as test_client:
-        with app.app_context():
-            yield test_client
 
 
 def test_get_stations(my_test_client):
@@ -28,31 +15,33 @@ def test_get_stations(my_test_client):
     assert len(data) == 10
 
 
-def test_create_station_success(my_test_client):
+def test_create_station_success(my_test_client, get_token):
     """
     GIVEN an app configured for testing
     WHEN the '/api/stations' is passed (POST) with valid station json data
     THEN check that the response is valid and has a new station data
     """
+
     station_data = {"name": "Toronto Union", "location": "Dundas and Young"}
     resp = my_test_client.post(
-        '/api/stations', json={**station_data})
+        '/api/stations', json={**station_data}, headers={"Authorization": "Bearer {}".format(get_token)})
     assert resp.status_code == 200
     data = resp.get_json()
     assert data['name'] == "Toronto Union"
 
 
-def test_create_station_error(my_test_client):
+def test_create_station_error(my_test_client, get_token):
     """
     GIVEN an app configured for testing
     WHEN the '/api/stations' is passed (POST) with empty data
     THEN check that the response is 400
     """
-    resp = my_test_client.post('/api/stations')
+    resp = my_test_client.post(
+        '/api/stations', headers={"Authorization": "Bearer {}".format(get_token)})
     assert resp.status_code == 400
 
 
-def test_update_station_success(my_test_client):
+def test_update_station_success(my_test_client, get_token):
     """
     GIVEN an app configured for testing
     WHEN the '/api/stations' is passed (POST) with station json data
@@ -60,13 +49,14 @@ def test_update_station_success(my_test_client):
     """
     _station = {'id': 1, 'name': 'updated station1',
                 'location': "Toronto", 'date_created': '2021-01-20 23:38:01'}
-    resp = my_test_client.post('/api/station/1', json={**_station})
+    resp = my_test_client.post('/api/station/1', json={**_station}, headers={
+                               "Authorization": "Bearer {}".format(get_token)})
     assert resp.status_code == 200
     data = resp.get_json()
     assert data['id'] == 1
 
 
-def test_update_station_error(my_test_client):
+def test_update_station_error(my_test_client, get_token):
     """
     GIVEN an app configured for testing
     WHEN the '/api/stations' is passed (POST) without date_created value
@@ -74,5 +64,6 @@ def test_update_station_error(my_test_client):
     """
     _station = {'id': 1, 'name': 'updated station1',
                 'location': "Toronto"}
-    resp = my_test_client.post('/api/station/1', json={**_station})
+    resp = my_test_client.post('/api/station/1', json={**_station}, headers={
+                               "Authorization": "Bearer {}".format(get_token)})
     assert resp.status_code == 422
